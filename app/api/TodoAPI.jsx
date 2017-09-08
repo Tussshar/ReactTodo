a8c269eddc2e7e018b9413754fb9bb2c5cb27457
Tussshar <tusharkale52@gmail.com>
2017-09-08 07:30:42 -0400

    Sec 9, LEct 100: Wiring Up Search

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -19,5 +19,49 @@ module.exports = {
     }
 
     return $.isArray(todos) ? todos : [];
+  },
+  filterTodos: function(todos, showCompleted, searchText) {
+    var filteredTodos = todos;
+
+    //Filter by showCompleted
+    /*
+      filter is a built in array method that lets you take an existing array
+      and filter it based on certain things. The filter method requires a
+      callback. Its first and only argument is a callback function. This callback
+      gets called for every item in an array. It gets passed individual item.
+      If return true then item would stay in array and if we return false
+      then the item would get removed from array
+    */
+    filteredTodos = filteredTodos.filter((todo) => {
+      return !todo.completed || showCompleted;
+    });
+
+    //Filter by searchText
+    /*
+      indexOf would return -1 if it wasnt found. If it is greater than -1 then it
+      is a success
+    */
+    filteredTodos = filteredTodos.filter((todo) => {
+      var todoText = todo.text.toLowerCase();
+
+      return searchText.length === 0 || todoText.indexOf(searchText);
+    });
+
+    //Sort todos with non-completed first
+    /*
+      sort is also an in built function.
+      both the arguments i.e. a and b are todos
+    */
+    filteredTodos.sort((a, b) => {
+      if(!a.completed && b.completed){
+        return -1;
+      } else if (a.completed && !b.completed) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+
+    return filteredTodos;
   }
 };
